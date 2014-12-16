f48c697425064d82602eb91f152dc925b10d17da
Florianschmidtwelzow <florian.schmidt.welzow@t-online.de>
2014-12-16 08:25:02 +0100

    Use $GLOBALS for globals
    Follow up: I10f071253e48e6492949ebbc3c07964c747e885f
    
    Change-Id: Iabcba276de1031041b2d458679b4ebca1931c129

--- Vector.php
+++ Vector.php
@@ -22,8 +22,8 @@
  * @ingroup Skins
  */
 
-if ( version_compare( $wgVersion, '1.25c', '<' ) ) {
-	echo 'This version of the Vector skin requires at least MediaWiki 1.25, you have ' . $wgVersion .
+if ( version_compare( $GLOBALS['wgVersion'], '1.25c', '<' ) ) {
+	echo 'This version of the Vector skin requires at least MediaWiki 1.25, you have ' . $GLOBALS['wgVersion'] .
 		'. You can download a more appropriate version here: https://www.mediawiki.org/wiki/Special:SkinDistributor/Vector';
 	exit();
 }
