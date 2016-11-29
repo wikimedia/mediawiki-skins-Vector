9bd5ce5a1e53a3e6347bc8eb8c29d3bd0605ad0f
James D. Forrester <jforrester@wikimedia.org>
2016-11-29 08:22:16 -0800

    build: Replace jshint and jscs with eslint
    Change-Id: I48d542580d767df2d17ce4c6668e9e233a0f7903

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,28 @@
-/*jshint node:true */
+/* eslint-env node */
 module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'skin.json' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
-	grunt.loadNpmTasks( 'grunt-jscs' );
 
 	grunt.initConfig( {
-		jshint: {
-			options: {
-				jshintrc: true
-			},
+		eslint: {
 			all: [
 				'*.js',
 				'**/*.js',
 				'!node_modules/**'
 			]
 		},
-		jscs: {
-			src: '<%= jshint.all %>'
-		},
-		banana: conf.MessagesDirs,
 		jsonlint: {
 			all: [
+				'*.json',
 				'**/*.json',
 				'!node_modules/**'
 			]
-		}
+		},
+		banana: conf.MessagesDirs
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
