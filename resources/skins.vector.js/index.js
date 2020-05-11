108393daf11e2d4364e695ed3ba2a456ad8dacb7
Stephen Niedzielski <stephen@niedzielski.com>
2020-05-11 15:29:56 -0600

    [dev][Legacy][JS] Split Legacy mode JavaScript into new ResourceLoader module
    The collapsible sidebar adds a new JavaScript dependency and behavior to
    Latest mode only. There are a number of ways of to make the deviation
    but we think now is the time to start splitting by module.
    
    This patch adds a new ResourceLoader module, skins.vector.legacy.js, and
    moves the existing JavaScript into it. The old module, skins.vector.js,
    has been given a currently matching index.js entry point that references
    the collapsible tabs' files by reaching across directories. It's not
    quite ideal as usually ResourceLoader modules and directory structures
    strive for 1:1 correspondence but this patch makes the bold assertions
    that it's better than a file copy, better than a new
    "skins.vector.common.js" ResourceLoader module, more compatible than a
    symlink, and the existing jQuery tabs implementation will eventually be
    replaced in Latest mode.
    
    A "Legacy" module was added instead of a "Latest" with the assumption
    that active development should generally be considered "latest" and
    Legacy an intentional distinction.
    
    Bug: T246419
    Change-Id: I9980403f1ee5897c27ac0331f0b51a5bcbdff778

--- resources/skins.vector.js/index.js
+++ resources/skins.vector.js/index.js
@@ -1,6 +1,6 @@
 var
-	collapsibleTabs = require( './collapsibleTabs.js' ),
-	vector = require( './vector.js' );
+	collapsibleTabs = require( '../skins.vector.legacy.js/collapsibleTabs.js' ),
+	vector = require( '../skins.vector.legacy.js/vector.js' );
 
 function main() {
 	collapsibleTabs.init();
