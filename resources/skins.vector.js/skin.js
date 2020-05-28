3003724a3a0d5bcc54aa1a71725531016136f874
Stephen Niedzielski <stephen@niedzielski.com>
2020-05-28 14:19:06 -0600

    [fix][Less] disable sidebar animations on page load
    There's a longstanding Chrome bug that causes CSS transitions to show in
    their terminal states. This patch works around the issue by limiting
    transitions to JavaScript users only via the `client-js` class which is
    added to the root `html` Node some time after page content loads. The
    effect is a better overall experience for everyone but transitions
    unfortunately no longer appear for no-JS devices.
    
    I am unable to reproduce this issue in Vector's configuration. This
    patch should only be merged as a last resort.
    
    Bug: T246419
    Bug: T234570
    Change-Id: Ifcb2bf1fddb85113a4858b7a210ded3954952e6e

--- resources/skins.vector.js/skin.js
+++ resources/skins.vector.js/skin.js
@@ -4,6 +4,38 @@ var collapsibleTabs = require( '../skins.vector.legacy.js/collapsibleTabs.js' );
 var vector = require( '../skins.vector.legacy.js/vector.js' );
 
 /**
+ * Wait for first paint before calling this function. That's its whole purpose.
+ *
+ * Some CSS animations and transitions are "disabled" by default as a workaround to this old Chrome
+ * bug, https://bugs.chromium.org/p/chromium/issues/detail?id=332189, which otherwise causes them to
+ * render in their terminal state on page load. By adding the `vector-animations-ready` class to the
+ * `html` root element **after** first paint, the animation selectors suddenly match causing the
+ * animations to become "enabled" when they will work properly. A similar pattern is used in Minerva
+ * (see T234570#5779890, T246419).
+ *
+ * Example usage in Less:
+ *
+ * ```less
+ * .foo {
+ *     color: #f00;
+ *     .transform( translateX( -100% ) );
+ * }
+ *
+ * // This transition will be disabled initially for JavaScript users. It will never be enabled for
+ * // no-JS users.
+ * .vector-animations-ready .foo {
+ *     .transition( transform 100ms ease-out; );
+ * }
+ * ```
+ *
+ * @param {Document} document
+ * @return {void}
+ */
+function enableCssAnimations( document ) {
+	document.documentElement.classList.add( 'vector-animations-ready' );
+}
+
+/**
  * Improve the interactivity of the sidebar panel by binding optional checkbox hack enhancements
  * for focus and `aria-expanded`. Also, flip the icon image on click.
  * @param {Document} document
@@ -25,6 +57,7 @@ function initSidebar( document ) {
  * @return {void}
  */
 function main( window ) {
+	enableCssAnimations( window.document );
 	collapsibleTabs.init();
 	$( vector.init );
 	initSidebar( window.document );
