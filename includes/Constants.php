49f2b25737aa59494ceb1318361f5a2c65e15d8f
Nicholas Ray <nray@wikimedia.org>
2021-04-28 18:47:25 -0600

    Create A/B test harness for Language in header feature
    * Adds ab test config to enable/disable the ab test. Defaults to `false`
    (ab test disabled).
    
    * Adds a `languageinheader` query param which only takes effect when the
    ab test is enabled. The query param is cast to a bool and determines
    which treatment is shown. For example, set query param to
    `languageinheader=1` to see the new treatment. Set query param to
    `languageinheader=0` to see the old treatment. To bucket based on the
    user's id or global user's id, don't set the query param.
    
    * Moves the language in header config work that was previously in
    ServiceWiring into a `LanguageInHeaderTreatmentRequirement` class so
    that unit tests can be done on most of the logic that determines whether
    the language in header will show.
    
    * Adds logic to bucket user based on [global] user id.
    
    Bug: T280825
    Change-Id: Id538fe6e09002fae6c371109769f3b7d61e7ac6d

--- includes/Constants.php
+++ includes/Constants.php
@@ -121,7 +121,30 @@ final class Constants {
 	 */
 	public const REQUIREMENT_LANGUAGE_IN_HEADER = 'LanguageInHeader';
 
+	/**
+	 * Defines whether or not the Language in header A/B test is running. See
+	 * https://phabricator.wikimedia.org/T280825 for additional detail about the test.
+	 *
+	 * Note well that if the associated config value is falsy, then we fall back to choosing the
+	 * language treatment based on the `VectorLanguageInHeader` config variable.
+	 *
+	 * @var string
+	 */
+	public const CONFIG_LANGUAGE_IN_HEADER_TREATMENT_AB_TEST = 'VectorLanguageInHeaderTreatmentABTest';
+
 	// These are used for query parameters.
+	/**
+	 * If undefined and AB test enabled, user will be bucketed as usual.
+	 *
+	 * If set, overrides the language in header AB test config:
+	 *
+	 * 'languageinheader=0' will show existing treatment.
+	 * 'languageinheader=1' will show new treatment.
+	 *
+	 * @var string
+	 */
+	public const QUERY_PARAM_LANGUAGE_IN_HEADER = 'languageinheader';
+
 	/**
 	 * Override the skin version user preference and site Config. See readme.
 	 * @var string
