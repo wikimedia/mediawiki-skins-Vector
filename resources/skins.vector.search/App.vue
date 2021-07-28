aebb3fb5222a88bece34d5c0cf0d792ccb3f7832
jdlrobson <jdlrobson@gmail.com>
2021-07-28 09:15:57 -0700

    Use the search title if configured
    Allow commons to use Special:MediaSearch
    
    Depends-On: 	I37b9d3a2b263f496a283f4bfdc769b7dc880ab06
    Bug: T287540
    Change-Id: I2a2463d704ef5b2264574cdf186836ba00a639f5

--- resources/skins.vector.search/App.vue
+++ resources/skins.vector.search/App.vue
@@ -10,6 +10,7 @@
 		:title="searchTitle"
 		:placeholder="searchPlaceholder"
 		:aria-label="searchPlaceholder"
+		:search-page-title="searchPageTitle"
 		:initial-input-value="searchQuery"
 		:button-label="$i18n( 'searchbutton' ).text()"
 		:form-action="action"
@@ -74,6 +75,10 @@ module.exports = {
 		}
 	},
 	props: {
+		searchPageTitle: {
+			type: String,
+			default: 'Special:Search'
+		},
 		autofocusInput: {
 			type: Boolean,
 			default: false
