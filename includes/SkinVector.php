029110810edacda5ad6092d4946c5594c75f9d62
Jon Robson <jdlrobson@gmail.com>
2022-01-27 14:25:30 -0800

    Language alert box should use Html::noticeBox
    Html::noticeBox doesn't output any deprecated classes so
    will not conflict with any user gadgets now.
    
    Bug: T299625
    Change-Id: Ic9aef5c8ff3fae6a79c7cf82cc3c97714cf40b3d

--- includes/SkinVector.php
+++ includes/SkinVector.php
@@ -673,9 +673,9 @@ class SkinVector extends SkinMustache {
 			// T295555 Add language switch alert message temporarily (to be removed).
 			if ( $this->shouldLanguageAlertBeInSidebar() && !$parentData['is-mainpage'] ) {
 				$languageSwitchAlert = [
-					'html-content' => Html::rawElement( 'div',
-						[ 'class' => 'messagebox' ],
-						$this->msg( 'vector-language-redirect-to-top' )->parse()
+					'html-content' => Html::noticeBox(
+						$this->msg( 'vector-language-redirect-to-top' )->parse(),
+						'vector-language-sidebar-alert'
 					),
 				];
 				$headingOptions = [
