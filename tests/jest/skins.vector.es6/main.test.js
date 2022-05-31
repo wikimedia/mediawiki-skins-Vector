b0312473366bf0ae6dd5ab83adf8da3a8bbbe597
Jon Robson <jdlrobson@gmail.com>
2022-05-31 15:32:50 -0700

    Adds tests for stickyHeader A/B test logic
    Follow up to I3effbb3e5f0bb1c8663255936458e3849511dfca
    
    Bug: T299959
    Change-Id: I8703587a6e616c426638e44ca6dad12617f10ae7

--- tests/jest/skins.vector.es6/main.test.js
+++ tests/jest/skins.vector.es6/main.test.js
@@ -1,5 +1,8 @@
 const { test } = require( '../../../resources/skins.vector.es6/main.js' );
-
+const {
+	STICKY_HEADER_EXPERIMENT_NAME,
+	STICKY_HEADER_EDIT_EXPERIMENT_NAME
+} = require( '../../../resources/skins.vector.es6/stickyHeader.js' );
 describe( 'main.js', () => {
 	it( 'getHeadingIntersectionHandler', () => {
 		const section = document.createElement( 'div' );
@@ -23,6 +26,98 @@ describe( 'main.js', () => {
 			const handler = test.getHeadingIntersectionHandler( fn );
 			handler( node );
 			expect( fn ).toHaveBeenCalledWith( testCase[ 1 ] );
+		} );
+	} );
+
+	it( 'initStickyHeaderABTests', () => {
+		const STICKY_HEADER_AB = {
+			name: STICKY_HEADER_EXPERIMENT_NAME,
+			enabled: true
+		};
+		const STICKY_HEADER_AB_EDIT = {
+			name: STICKY_HEADER_EDIT_EXPERIMENT_NAME,
+			enabled: true
+		};
+		[
+			{
+				abConfig: STICKY_HEADER_AB_EDIT,
+				isEnabled: false,
+				isUserInTreatmentBucket: false,
+				expectedResult: {
+					showStickyHeader: false,
+					disableEditIcons: true
+				}
+			},
+			{
+				abConfig: STICKY_HEADER_AB_EDIT,
+				isEnabled: true,
+				isUserInTreatmentBucket: false,
+				expectedResult: {
+					showStickyHeader: true,
+					disableEditIcons: true
+				}
+			},
+			{
+				abConfig: STICKY_HEADER_AB_EDIT,
+				isEnabled: true,
+				isUserInTreatmentBucket: true,
+				expectedResult: {
+					showStickyHeader: true,
+					disableEditIcons: false
+				}
+			},
+			{
+				abConfig: STICKY_HEADER_AB,
+				isEnabled: false, // sticky header unavailable
+				isUserInTreatmentBucket: false, // not in treament bucket
+				expectedResult: {
+					showStickyHeader: false,
+					disableEditIcons: true
+				}
+			},
+			{
+				abConfig: STICKY_HEADER_AB,
+				isEnabled: true, // sticky header available
+				isUserInTreatmentBucket: false, // not in treament bucket
+				expectedResult: {
+					showStickyHeader: false,
+					disableEditIcons: true
+				}
+			},
+			{
+				abConfig: STICKY_HEADER_AB,
+				isEnabled: false, // sticky header is not available
+				isUserInTreatmentBucket: true, // but the user is in the treament bucket
+				expectedResult: {
+					showStickyHeader: false,
+					disableEditIcons: true
+				}
+			},
+			{
+				abConfig: STICKY_HEADER_AB,
+				isEnabled: true,
+				isUserInTreatmentBucket: true,
+				expectedResult: {
+					showStickyHeader: true,
+					disableEditIcons: true
+				}
+			}
+		].forEach( ( { abConfig, isEnabled, isUserInTreatmentBucket, expectedResult } ) => {
+			document.documentElement.classList.add( 'vector-sticky-header-enabled' );
+			const result = test.initStickyHeaderABTests(
+				abConfig,
+				isEnabled,
+				( experiment ) => ( {
+					name: experiment.name,
+					isInBucket: () => true,
+					isInSample: () => true,
+					getBucket: () => 'bucket',
+					isInTreatmentBucket: () => {
+						return isUserInTreatmentBucket;
+					}
+				} )
+			);
+			expect( result ).toMatchObject( expectedResult );
 		} );
 	} );
 } );
