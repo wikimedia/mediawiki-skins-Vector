7842d15382112d6359053ebf89a1536b32bafa81
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2023-01-05 13:37:48 +0100

    Remove code to disable new search on Wikidata
    This should be a no-op since Wikibase change I6a7a42a64c (commit
    19267799da), which enables the wikibase.vector.searchClient module on
    wikidatawiki as well; assuming no problems turn up when that is
    deployed, we should be able to merge this soon afterwards.
    
    Bug: T316093
    Change-Id: I8d7aaf67d98cc015d94bb6aa1c71a92905dad024
    Follows-Up: I0aa0e432181b14cdb7b92e2550b78f2d7d48094d

--- resources/skins.vector.js/searchLoader.js
+++ resources/skins.vector.js/searchLoader.js
@@ -138,9 +138,7 @@ function markLoadEnd( startMarker, endMarker, measureMarker ) {
  * @param {Document} document
  */
 function initSearchLoader( document ) {
-	var searchBoxes = document.querySelectorAll( '.vector-search-box' ),
-		wikibaseNeedsLegacySearch = [ 'loading', 'loaded', 'executing', 'ready' ]
-			.indexOf( mw.loader.getState( 'wikibase.ui.entitysearch' ) ) !== -1;
+	var searchBoxes = document.querySelectorAll( '.vector-search-box' );
 
 	// Allow developers to defined $wgVectorSearchApiUrl in LocalSettings to target different APIs
 	if ( config.wgVectorSearchApiUrl ) {
@@ -152,10 +150,9 @@ function initSearchLoader( document ) {
 	}
 
 	/**
-	 * 1. If we are in a browser that doesn't support ES6 fall back to non-JS version.
-	 * 2. Disable on Wikidata per T281318 until the REST API is ready.
+	 * If we are in a browser that doesn't support ES6 fall back to non-JS version.
 	 */
-	if ( wikibaseNeedsLegacySearch || mw.loader.getState( 'skins.vector.search' ) === null ) {
+	if ( mw.loader.getState( 'skins.vector.search' ) === null ) {
 		document.body.classList.remove(
 			'skin-vector-search-vue'
 		);
