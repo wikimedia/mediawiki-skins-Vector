e4af2b5df33a9f7104bf67842125f4f93874a4fc
Jan Drewniak <jan.drewniak@gmail.com>
2023-02-14 09:58:54 -0500

    Prevent addPortletLinkHandler from looping over links twice
    - Prevents the addPortletLinkHandler function from looping
    over the same links multiple times by indexing
    the HTMLElement instead of just the ID, which is an
    optional parameter.
    - Conditionally adds the icon class if the ID parameter
    exists.
    - Refactors the function to calculate the width of the
    vector toolbar.
    - Adds Jest tests to account for the dual icon scenario
    and updates existing Jest tests.
    
    Bug: T327369
    Change-Id: I2a0934405efebd0e95919bc523d711866236a7e6

--- resources/skins.vector.js/dropdownMenus.js
+++ resources/skins.vector.js/dropdownMenus.js
@@ -60,7 +60,43 @@ function createIconElement( menuElement, parentElement, id ) {
 	return iconElement;
 }
 
-var /** @type {Object} */handledLinks = {};
+/**
+ * Calculate the available width for adding links in the veiws menu,
+ * i.e. the remaining space in the toolbar between the right-navigation
+ * and left-navigation elements.
+ *
+ * @return {number} remaining available pixels in page toolbar or Zero
+ *                  if remaining space is negative.
+ */
+function getAvailableViewMenuWidth() {
+	var
+		// Vector toolbar containing namespace, views, more menu etc.
+		toolbar = document.querySelector( '.vector-page-toolbar-container' ),
+		// Assumes all left-side menus are wrapped in a single nav element.
+		// Need to get child width since this node is flex-grow: 1;
+		leftToolbarItems = document.querySelector( '#left-navigation > nav' ),
+		// Right side elements are flex-grow:0 so top-level width is sufficient.
+		rightToolbarItems = document.getElementById( 'right-navigation' );
+
+	// Views menu collapses into "more" menu at this resolution.
+	// Move the link from views to actions menu in this situation.
+	if ( window.innerWidth < 720 ) {
+		return 0;
+	}
+
+	// If any of our assumption about the DOM are wrong, return 0
+	// in order to place the link in a known menu instead.
+	if ( !( toolbar && leftToolbarItems && rightToolbarItems ) ) {
+		return 0;
+	}
+
+	// returning zero instead of negative number makes boolean conversion easier.
+	return Math.max( 0,
+		toolbar.clientWidth - leftToolbarItems.clientWidth - rightToolbarItems.clientWidth
+	);
+}
+
+var /** @type {Array<HTMLElement>} */handledLinks = [];
 
 /**
  * Adds icon placeholder for gadgets to use.
@@ -73,28 +109,35 @@ var /** @type {Object} */handledLinks = {};
  * @param {PortletLinkData} data
  */
 function addPortletLinkHandler( item, data ) {
-	var link = item.querySelector( 'a' );
-	var $menu = $( item ).parents( '.vector-menu' );
-	var menuElement = $menu.length && $menu.get( 0 ) || null;
-	var iconElement = createIconElement( menuElement, link, data.id );
-	// @ts-ignore see gerrit:836861
-	if ( data.id && handledLinks[ data.id ] ) {
+	var
+		link,
+		$menu,
+		menuElement,
+		linkIsHandled = handledLinks.indexOf( item ),
+		iconElement;
+
+	if ( linkIsHandled >= 0 ) {
 		return;
+	} else {
+		handledLinks.push( item );
+	}
+
+	// assign variables after early return.
+	link = item.querySelector( 'a' );
+	$menu = $( item ).parents( '.vector-menu' );
+	menuElement = $menu.length && $menu.get( 0 ) || null;
+
+	if ( data.id ) {
+		iconElement = createIconElement( menuElement, link, data.id );
 	}
 
 	// The views menu has limited space so we need to decide whether there is space
-	// to accomodate the new item and if not to redirect to the more dropdown.
-	/* eslint-disable no-jquery/no-global-selector */
+	// to accommodate the new item and if not to redirect to the more dropdown.
 	if ( $menu.prop( 'id' ) === 'p-views' ) {
-		// @ts-ignore if undefined as NaN will be ignored
-		var availableWidth = $( '.vector-page-toolbar-container' ).width() -
-			// @ts-ignore
-			$( '#p-namespaces' ).width() - $( '#p-variants' ).width() -
-			// @ts-ignore
-			$( '#p-views' ).width() - $( '#p-cactions' ).width();
+		var availableWidth = getAvailableViewMenuWidth();
 		var moreDropdown = document.querySelector( '#p-cactions ul' );
-		// If the screen width is less than 720px then the views menu is hidden
-		if ( moreDropdown && ( availableWidth < 0 || window.innerWidth < 720 ) ) {
+
+		if ( moreDropdown && !availableWidth ) {
 			moreDropdown.appendChild( item );
 			// reveal if hidden
 			mw.util.showPortlet( 'p-cactions' );
@@ -104,12 +147,6 @@ function addPortletLinkHandler( item, data ) {
 	// Check link.prepend exists for older browser since this is ES5 code
 	if ( link && iconElement && link.prepend ) {
 		link.prepend( iconElement );
-	}
-
-	// Mark the link as handled.
-	if ( data.id ) {
-		// @ts-ignore see gerrit:836861
-		handledLinks[ data.id ] = true;
 	}
 }
 
