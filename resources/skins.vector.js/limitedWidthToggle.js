197e4f45ed941cdaa12f76e87cf20d90a4c6ef85
bwang <bwang@wikimedia.org>
2023-05-22 13:26:29 -0500

    Remove limited toggle notification on page load
    Also removes VectorLimitedWidthIndicator config
    
    Bug: T336197
    Change-Id: I6005c0cf4933647d5dda3df2c95650aca3fde5cd

--- resources/skins.vector.js/limitedWidthToggle.js
+++ resources/skins.vector.js/limitedWidthToggle.js
@@ -1,8 +1,6 @@
 const features = require( './features.js' );
 const popupNotification = require( './popupNotification.js' );
-const config = require( './config.json' );
 const LIMITED_WIDTH_FEATURE_NAME = 'limited-width';
-const AWARE_COOKIE_NAME = `${LIMITED_WIDTH_FEATURE_NAME}-aware`;
 const TOGGLE_ID = 'toggleWidth';
 
 /**
@@ -42,23 +40,6 @@ function init() {
 	}
 
 	setDataAttribute( toggle );
-	const userMayNotKnowTheyAreInExpandedMode = !mw.cookie.get( AWARE_COOKIE_NAME );
-	const dismiss = () => {
-		mw.cookie.set( AWARE_COOKIE_NAME, '1' );
-	};
-	/**
-	 * check user has not disabled cookies by
-	 * reading the cookie and unsetting the cookie.
-	 *
-	 * @return {boolean}
-	 */
-	const areCookiesEnabled = () => {
-		dismiss();
-		// @ts-ignore https://github.com/wikimedia/typescript-types/pull/39
-		const savedSuccessfully = mw.cookie.get( AWARE_COOKIE_NAME ) === '1';
-		mw.cookie.set( AWARE_COOKIE_NAME, null );
-		return savedSuccessfully;
-	};
 
 	/**
 	 * @param {string} id this allows us to group notifications making sure only one is visible
@@ -66,27 +47,12 @@ function init() {
 	 * @param {number|false} timeout
 	 */
 	const showPopup = ( id, timeout = 4000 ) => {
-		if ( !config.VectorLimitedWidthIndicator ) {
-			return;
-		}
-		popupNotification.add( settings, getPopupText(), id, [], timeout, dismiss )
+		popupNotification.add( settings, getPopupText(), id, [], timeout )
 			.then( ( popupWidget ) => {
 				if ( popupWidget ) {
 					popupNotification.show( popupWidget, timeout );
 				}
 			} );
-	};
-
-	/**
-	 * FIXME: This currently loads OOUI on page load. It should be swapped out
-	 * for a more performance friendly version before being deployed.
-	 * See T334366.
-	 */
-	const showPageLoadPopups = () => {
-		if ( !config.VectorLimitedWidthIndicator ) {
-			return;
-		}
-		addLimitedWidthPopup( settings, getPopupText(), dismiss );
 	};
 
 	toggle.addEventListener( 'click', function () {
@@ -104,47 +70,6 @@ function init() {
 			showPopup( TOGGLE_ID );
 		}
 	} );
-
-	if ( userMayNotKnowTheyAreInExpandedMode ) {
-		if ( areCookiesEnabled() ) {
-			showPageLoadPopups();
-		}
-	}
-}
-/**
- * @param {HTMLElement} container
- * @param {string} message
- * @param {Function} [onDismiss]
- */
-function addLimitedWidthPopup( container, message, onDismiss = () => {} ) {
-	const popupTemplateString = `
-		<div class="vector-limited-width-popup">
-			<div class="vector-limited-width-popup-head">
-				<button class="vector-limited-width-popup-close-button mw-ui-button mw-ui-quiet mw-ui-icon-element">
-					<span class="mw-ui-icon mw-ui-icon-wikimedia-close"></span>
-					<span>Close</span>
-				</button>
-			</div>
-			<div class="vector-limited-width-popup-body">
-				<p>${message}</p>
-			</div>
-			<div class="vector-limited-width-popup-anchor"></div>
-		</div>
-	`;
-	const popupFrag = document.createRange().createContextualFragment( popupTemplateString );
-	container.appendChild( popupFrag );
-
-	const closePopup = () => {
-		const popup = /** @type {HTMLElement} */ ( document.querySelector( '.vector-limited-width-popup' ) );
-		if ( popup && popup.parentElement ) {
-			popup.parentElement.removeChild( popup );
-		}
-		onDismiss();
-	};
-	const closeButton = /** @type {HTMLElement} */ ( document.querySelector( '.vector-limited-width-popup-close-button' ) );
-	closeButton.addEventListener( 'click', closePopup, { once: true } );
-	const toggle = /** @type {HTMLElement} */ ( document.querySelector( '.vector-limited-width-toggle' ) );
-	toggle.addEventListener( 'click', closePopup, { once: true } );
 }
 
 module.exports = init;
