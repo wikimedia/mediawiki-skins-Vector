735dad995b1db59d9be1767b2999a46ae050f315
Jon Robson <jdlrobson@gmail.com>
2023-10-02 14:03:21 -0700

    Preferences set to "disabled" return false when isEnabled is called
    The font size preference sets a preference to disabled rather than 0.
    
    Rather than save this as 0, it would be useful to store this as a string
    given in future it will evolve to have values small, medium, large
    
    Change logic in UserPreferenceRequirement to support non-binary options.
    
    Bug: T346987
    Change-Id: I399aa1b1be4a45cab0aa3b8addb482e5af6c5bf3

--- tests/phpunit/unit/FeatureManagement/Requirements/UserPreferenceRequirementTest.php
+++ tests/phpunit/unit/FeatureManagement/Requirements/UserPreferenceRequirementTest.php
@@ -17,7 +17,7 @@ final class UserPreferenceRequirementTest extends \MediaWikiUnitTestCase {
 		return [
 			[
 				// Is option enabled?
-				true,
+				1,
 				// Is title present?
 				true,
 				// Expected
@@ -26,7 +26,7 @@ final class UserPreferenceRequirementTest extends \MediaWikiUnitTestCase {
 			],
 			[
 				// Is option enabled?
-				false,
+				0,
 				// Is title present?
 				true,
 				// Expected
@@ -35,12 +35,36 @@ final class UserPreferenceRequirementTest extends \MediaWikiUnitTestCase {
 			],
 			[
 				// Is option enabled?
-				true,
+				'enabled',
 				// Is title present?
 				false,
 				// Expected
 				false,
 				'If enabled but title null, returns false'
+			],
+			[
+				'disabled',
+				// Is title present?
+				true,
+				// Expected
+				false,
+				'If disabled, returns false'
+			],
+			[
+				'0',
+				// Is title present?
+				true,
+				// Expected
+				false,
+				'If disabled, returns false'
+			],
+			[
+				'medium',
+				// Is title present?
+				true,
+				// Expected
+				true,
+				'If unrecognized string returns true'
 			],
 		];
 	}
@@ -64,7 +88,6 @@ final class UserPreferenceRequirementTest extends \MediaWikiUnitTestCase {
 
 		$userOptionsLookup = $this->createMock( UserOptionsLookup::class );
 		$userOptionsLookup->method( 'getOption' )->willReturn( $isEnabled );
-		$userOptionsLookup->method( 'getBoolOption' )->willReturn( $isEnabled );
 
 		$requirement = new UserPreferenceRequirement(
 			$user,
