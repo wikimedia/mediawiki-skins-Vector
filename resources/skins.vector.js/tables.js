caa650a881ad5deff0909e476280f5c7ab044396
Jon Robson <jdlrobson@gmail.com>
2024-05-28 14:36:07 -0700

    Limit responsive tables to .wikitables
    Bug: T330527
    Change-Id: I61280270c6257ceaf5a6d74bb01be37063474bc7

--- resources/skins.vector.js/tables.js
+++ resources/skins.vector.js/tables.js
@@ -1,9 +1,13 @@
 const init = () => {
-	const tables = document.querySelectorAll( '.mw-parser-output table' );
+	const tables = document.querySelectorAll( '.mw-parser-output > table' );
 	Array.from( tables ).forEach( ( table ) => {
 		// Don't wrap tables within tables
 		const parent = table.parentElement;
-		if ( parent && !parent.matches( '.noresize' ) && !parent.closest( 'table' ) ) {
+		if (
+			parent && table.matches( '.wikitable' ) &&
+			!parent.matches( '.noresize' ) &&
+			!parent.closest( 'table' )
+		) {
 			const wrapper = document.createElement( 'div' );
 			wrapper.classList.add( 'noresize' );
 			parent.insertBefore( wrapper, table );
